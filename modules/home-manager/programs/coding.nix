ac366f77a77727a7415a75b3b555e4335fec1988
Bob Muit <bob@bobmuit.nl>
2025-03-15 12:10:40 +0100

    feat vscode extensions

--- modules/home-manager/programs/coding.nix
+++ modules/home-manager/programs/coding.nix
@@ -16,8 +16,10 @@
       extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide           # Nix language support extension
         reditorsupport.r              # R Language
-        zaaack.markdown-editor  # Add the Markdown Editor extension
+        # zaaack.markdown-editor  # Add the Markdown Editor extension
         redhat.vscode-yaml           # Add the YAML support extension
+        yzhang.markdown-all-in-one   # Markdown All in One extension
+        unicode-insert.unicode-insert # Unicode Insert extension
       ];
       userSettings = {
         "nix.enableLanguageServer" = true;  # Enable Nix language server
