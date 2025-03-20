48bc464304d212ce8b2ee2314e268a718a14e981
Bob Muit <bob@bobmuit.nl>
2025-03-20 20:07:44 +0100

    added u nixos to docker group

--- hosts/nixos-pi/default.nix
+++ hosts/nixos-pi/default.nix
@@ -54,7 +54,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nixos = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "podman"]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker"]; # Enable ‘sudo’ for the user.
     initialPassword = "nixos"; # Will be used if SSH fails; change this!
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGc0lUn+zcewulF+LE8+j87Gb3lKVZkBHZcoPRmpsV0j"
