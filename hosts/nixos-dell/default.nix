535c64870e4f5364a0e2113fe46287bf13ed240f
Bob Muit <bob@bobmuit.nl>
2025-04-25 20:05:15 +0200

    feat flatpak

--- hosts/nixos-dell/default.nix
+++ hosts/nixos-dell/default.nix
@@ -232,7 +232,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
-  # services.flatpak.enable = true;
+  services.flatpak.enable = true;
 
   # Autoupdate
   # system.autoUpgrade = {
