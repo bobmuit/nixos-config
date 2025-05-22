2b170fa6a5e359cd2bf11be17ba8105d93932a25
Bob Muit <bob@bobmuit.nl>
2025-05-22 09:09:56 +0200

    chore wayland explicitly enabled

--- hosts/nixos-dell/default.nix
+++ hosts/nixos-dell/default.nix
@@ -179,6 +179,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Disable unnecessary GNOME services
