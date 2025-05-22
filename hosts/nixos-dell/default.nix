7ad70f4fa1c3b89560ce44ad796c11619f020405
Bob Muit <bob@bobmuit.nl>
2025-05-22 08:55:14 +0200

    feat wayland

--- hosts/nixos-dell/default.nix
+++ hosts/nixos-dell/default.nix
@@ -179,7 +179,6 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Disable unnecessary GNOME services
